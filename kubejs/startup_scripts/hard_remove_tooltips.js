beeecf687b7b85c726ca78c44d8995bb3a7e0521
Cassian <cassian@godsted.com>
2026-02-25 16:38:00 -0500

    Reliable Replacer configs

--- kubejs/startup_scripts/hard_remove_tooltips.js
+++ kubejs/startup_scripts/hard_remove_tooltips.js
@@ -9,17 +9,8 @@ if ($KubeJS.startupScriptManager.firstLoad) {
 
 // blacklisted tooltips
 const blacklist = [
-	"shieldexp.tooltip.instructions",
-	"shieldexp.tooltip.instructions.parry",
-	"shieldexp.tooltip.attribute.stamina",
-	"shieldexp.tooltip.attribute.speed_factor_slow",
-	"shieldexp.tooltip.attribute.speed_factor_fast",
-	"shieldexp.tooltip.attribute.parry_damage",
 	"tooltip.storagedrawers.drawers.capacity",
 	"item.storagedrawers.drawer_key.desc",
-	"tooltip.toms_storage.hold_shift_for_info",
-	"tooltip.toms_storage.storage_terminal",
-	"tooltip.toms_storage.inventory_connector"
 ];
 
 // remove blacklisted tooltips
